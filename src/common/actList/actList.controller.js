926d668f4a4f1f15378fccbadb3cd1a7a73fa156
Victor Castaño <victorcastano@MacBook-Pro-de-Victor.local>
2017-03-08 20:22:05 +0100

    Lista de actividades preeliminar

--- src/common/actList/actList.controller.js
+++ src/common/actList/actList.controller.js
@@ -6,71 +6,66 @@ angular.module('common')
 
 activityListController.$inject = ['list', '$timeout'];
 function activityListController(list, $timeout) {
+
   var $ctrl = this;
 
-  $ctrl.list = list;
-  $ctrl.PAGE_SIZE = 10;
+  var initialLoad = ;
 
-  var DynamicItems = function() {
-    /**
-     * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
-     */
-    this.loadedPages = {};
+  this.infiniteItems = {
+    // Primera cantidad precargada al cargar la página
+    numLoaded_: list.length,
+    toLoad_: 0,
 
-    /** @type {number} Total number of items. */
-    this.numItems = 0;
+    getItemAtIndex: function(index) {
+      if (index > this.numLoaded_) {
+        this.fetchMoreItems_(index);
+        return null;
+      }
 
-    /** @const {number} Number of items to fetch per request. */
-    this.PAGE_SIZE = 50;
+      return index;
+    },
 
-    this.fetchNumItems_();
-  };
+    getLength: function() {
+      return this.numLoaded_ + 5;
+    },
 
-  // Required.
-  DynamicItems.prototype.getItemAtIndex = function(index) {
-    var pageNumber = Math.floor(index / this.PAGE_SIZE);
-    var page = this.loadedPages[pageNumber];
-
-    if (page) {
-      return page[index % this.PAGE_SIZE];
-    } else if (page !== null) {
-      this.fetchPage_(pageNumber);
+    fetchMoreItems_: function(index) {
+      if (this.toLoad_ < index) {
+        this.toLoad_ += 20;
+        $timeout(angular.noop, 300).then(angular.bind(this, function() {
+          this.numLoaded_ = this.toLoad_;
+        }));
+      }
     }
   };
 
-  // Required.
-  DynamicItems.prototype.getLength = function() {
-    return this.numItems;
-  };
+    /*this.infiniteItems = {
+      numLoaded_: 0,
+      toLoad_: 0,
 
-  DynamicItems.prototype.fetchPage_ = function(pageNumber) {
-    // Set the page to null so we know it is already being fetched.
-    this.loadedPages[pageNumber] = null;
+      getItemAtIndex: function(index) {
+        if (index > this.numLoaded_) {
+          this.fetchMoreItems_(index);
+          return null;
+        }
 
-    // For demo purposes, we simulate loading more items with a timed
-    // promise. In real code, this function would likely contain an
-    // $http request.
-    $timeout(angular.noop, 300).then(angular.bind(this, function() {
-      this.loadedPages[pageNumber] = [];
-      var pageOffset = pageNumber * this.PAGE_SIZE;
-      for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
-        this.loadedPages[pageNumber].push(i);
+        return index;
+      },
+
+      getLength: function() {
+        return this.numLoaded_ + 5;
+      },
+
+      fetchMoreItems_: function(index) {
+
+        if (this.toLoad_ < index) {
+          this.toLoad_ += 20;
+          $timeout(angular.noop, 300).then(angular.bind(this, function() {
+            this.numLoaded_ = this.toLoad_;
+          }));
+        }
       }
-    }));
-  };
-
-  DynamicItems.prototype.fetchNumItems_ = function() {
-    // For demo purposes, we simulate loading the item count with a timed
-    // promise. In real code, this function would likely contain an
-    // $http request.
-    $timeout(angular.noop, 300).then(angular.bind(this, function() {
-      this.numItems = 50000;
-    }));
-  };
-
-  this.dynamicItems = new DynamicItems();
-
-
+    };*/
 }
 
 })();
