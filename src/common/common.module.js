31abbd937bb99db9609ae168ff107c51959cdf26
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-04-29 23:42:34 +0200

    Añadido mensaje informativo de fichero de track en edición de actividades

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -2,8 +2,8 @@
 "use strict";
 
 angular.module('common', ['base64', 'ngStorage', 'ui.router'])
-//.constant('SERVER_URL', 'https://ocamserver.herokuapp.com')
-.constant('SERVER_URL', 'https://localhost:8443')
+.constant('SERVER_URL', 'https://ocamserver.herokuapp.com')
+//.constant('SERVER_URL', 'https://localhost:8443')
 .constant('LOGIN_API', '/api/auth/login')
 .constant('REGISTER_API', '/hiker')
 .constant('PENDING_ACTIVITIES_API', '/pendingActivities')
