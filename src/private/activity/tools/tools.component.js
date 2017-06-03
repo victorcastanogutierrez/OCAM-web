90debe12371cc392f02a8177ff4098fb7b8cb13c
Víctor Castaño Gutiérrez <victorcastanogutierrez@gmail.com>
2017-06-03 13:01:17 +0200

    Mejora de usabilidad sacando botones de acción sobre actividad a mayor visibilidad

--- src/private/activity/tools/tools.component.js
+++ src/private/activity/tools/tools.component.js
@@ -20,11 +20,11 @@ function ToolsController($scope, $timeout, $state, activityService,
   $mdDialog) {
   var $ctrl = this;
 
-  var editActivity = function() {
+  $ctrl.editActivity = function() {
     $state.go("private.newActivity", {activity: $ctrl.activity});
   };
 
-  var deleteActivity = function() {
+  $ctrl.deleteActivity = function() {
     $ctrl.onLoading({loading : true});
 
     var success = function() {
@@ -49,23 +49,15 @@ function ToolsController($scope, $timeout, $state, activityService,
     });
   }
 
-  var isActivityPending = function() {
+  $ctrl.isActivityPending = function() {
     return $ctrl.activity && $ctrl.activity.status == 'PENDING';
   };
 
-  var isActivityClosed = function() {
+  $ctrl.isActivityClosed = function() {
     return $ctrl.activity && $ctrl.activity.status == 'CLOSED';
   };
 
   $ctrl.edit = true;
-  $ctrl.items = [
-    { name: "Editar", icon: "mode_edit", direction: "bottom",
-      action: editActivity, disabled: isActivityClosed()},
-    { name: "Monitorizar", icon: "visibility", direction: "top",
-      action: $ctrl.monitorizar, disabled: isActivityClosed()},
-    { name: "Eliminar", icon:"delete_forever", direction: "bottom",
-      action: deleteActivity, disabled: !isActivityPending() || isActivityClosed()}
-  ];
   $ctrl.hidden = false;
   $ctrl.isOpen = false;
   $ctrl.hover = false;
